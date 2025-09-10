b1adc7d6e9d861b25d4587fe6bdc557d00acd413
Josh Bagwell <jbag1489@gmail.com>
2025-09-10 00:11:03 -0500

    Update bed sizes with creality sizes

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1724,15 +1724,15 @@
 // @section geometry
 
 // The size of the printable area
-#define X_BED_SIZE 358
-#define Y_BED_SIZE 370
+#define X_BED_SIZE 360
+#define Y_BED_SIZE 360
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
-#define X_MIN_POS 8
-#define Y_MIN_POS -1
+#define X_MIN_POS 0
+#define Y_MIN_POS 0
 #define Z_MIN_POS 0
-#define X_MAX_POS X_BED_SIZE + X_MIN_POS
-#define Y_MAX_POS Y_BED_SIZE + Y_MIN_POS
+#define X_MAX_POS X_BED_SIZE
+#define Y_MAX_POS Y_BED_SIZE
 #define Z_MAX_POS 405        + Z_MIN_POS
 //#define I_MIN_POS 0
 //#define I_MAX_POS 50
